db14cd47dee5d7c8e7c2428f6ad2034ee1380c9a
blueberry <dearbblueberry@gmail.com>
2025-10-13 00:30:15 +1100

    Add event stubs; add initial Peak assets and data; add initial Rei SkiTrip event.

---
+++ [CP] Hikawa/assets/Locations/HikawaPeak.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="HikawaPeak" tilewidth="16" tileheight="16" tilecount="1200" columns="30">
+ <image source="HikawaPeak.png" width="480" height="640"/>
+</tileset>
