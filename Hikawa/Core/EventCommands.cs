cce65ff7fb6f3433be0eef308bf211cd67562052
blueberry <dearbblueberry@gmail.com>
2025-10-26 13:05:52 +1100

    Draft Noodles event assets, script, and implementation.

--- Hikawa/Core/EventCommands.cs
+++ Hikawa/Core/EventCommands.cs
@@ -1,4 +1,5 @@
 ﻿using Hikawa.Objects.Events;
+using StardewModdingAPI.Events;
 using StardewValley.Delegates;
 using System;
 using System.Reflection;
@@ -39,6 +40,130 @@ public static class EventCommands
         {
             e.currentCustomEventScript = new CrystalBall();
             Game1.globalFadeToClear(afterFade: null, fadeSpeed: 0.01f);
+        }
+    }
+
+    [EventCommandAttribute("Noodles")]
+    private static void Noodles(Event e, string[] args, EventContext context)
+    {
+        float ms = (float)Game1.currentGameTime.TotalGameTime.TotalMilliseconds;
+        Game1.CurrentEvent.float_useMeForAnything = ms;
+
+        e.CurrentCommand++;
+        switch (e.int_useMeForAnything++)
+        {
+            case 0:
+                Game1.CurrentEvent.int_useMeForAnything2 = (int)ms;
+                ModEntry.Instance.Helper.Events.Display.RenderedWorld += draw;
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                break;
+            case 6:
+                Game1.CurrentEvent.int_useMeForAnything2 = (int)ms;
+                break;
+            default:
+                ModEntry.Instance.Helper.Events.Display.RenderedWorld -= draw;
+                break;
+        }
+
+        void draw(object sender, RenderedWorldEventArgs e)
+        {
+            SpriteBatch b = e.SpriteBatch;
+
+            float ms = (float)Game1.currentGameTime.TotalGameTime.TotalMilliseconds;
+            float interval = MathF.PI * 1000;
+
+            bool[] big = [false, true, true, false, false, true];
+            Rectangle[][] ingredients = [
+                [],
+                [
+                    // fishcake
+                    new(0, 1184, 32, 32)
+                ],
+                [
+                    // seaweed
+                    new(64, 1184, 32, 32)
+                ],
+                [
+                    // spring onions
+                    new(96, 1184, 16, 16), new(96, 1200, 16, 16), new(112, 1184, 16, 16)
+                ],
+                [
+                    // frozen veg
+                    new(112, 1200, 16, 16), new(128, 1184, 16, 16), new(128, 1200, 16, 16), new(128, 1200, 16, 16), new(144, 1200, 16, 16), new(144, 1200, 16, 16), new(160, 1200, 16, 16), new(160, 1200, 16, 16)
+                ],
+                [
+                    // egg
+                    new(32, 1184, 32, 32)
+                ]
+            ];
+
+            float overlayStartMs = Game1.CurrentEvent.int_useMeForAnything2;
+            float overlayFadeTime = 1000;
+            float overlayFadeMs = Math.Min(ms - overlayStartMs, overlayFadeTime);
+            float overlayFadeRatio = overlayFadeMs / overlayFadeTime;
+
+            float spriteStartMs = Game1.CurrentEvent.float_useMeForAnything;
+            float spriteFadeTime = 1000;
+            float spriteFadeMs = Math.Min(ms - spriteStartMs, spriteFadeTime);
+            float spriteFadeRatio = spriteFadeMs / spriteFadeTime;
+
+            int stage = Game1.CurrentEvent.int_useMeForAnything;
+            stage = Math.Clamp(stage + (spriteFadeRatio < 0.5f ? -1 : 0), 0, ingredients.Length);
+
+            float spriteAlpha = 1f * Utils.CircularFromRatio(spriteFadeRatio); //(MathF.Sin(0.5f + 0.5f * MathF.PI * spriteFadeRatio));
+            float overlayAlpha = 1f * Utils.CircularFromRatio(0.5f * overlayFadeRatio * (stage < ingredients.Length - 1 ? 1 : -1));
+            float scale = Game1.pixelZoom;
+
+            // Draw overlay
+            b.Draw(Game1.staminaRect, new Rectangle(0, 0, Game1.viewport.Width, Game1.viewport.Height), Color.PaleVioletRed * overlayAlpha);
+
+            // Redraw actors above overlay and sprites
+            Game1.player.draw(b);
+            foreach (var actor in Game1.CurrentEvent.actors)
+                actor.draw(b);
+
+            if (stage <= 0 || stage >= ingredients.Length)
+                return;
+
+            int mult = big[stage] ? 6 : 3;
+            int w = Game1.viewport.Width;
+            int h = Game1.viewport.Height;
+            int countX = w / Game1.tileSize / mult;
+            int countY = h / Game1.tileSize / mult + 1;
+
+            // Draw ingredient sprites
+            for (int x = 0; x < countX; ++x)
+            {
+                for (int y = 0; y < countY; ++y)
+                {
+                    float ratio = ((ms + (x + y) * 1000) % interval) / interval;
+                    float circular = Utils.SharpCircularFromRatio(ratio);
+
+                    Rectangle[] sources = ingredients[stage];
+                    Rectangle source = sources[(x + y) % sources.Length];
+                    Vector2 position = new Vector2(w * x / w * Game1.tileSize * mult, h * y / h * Game1.tileSize * mult * 1.25f);
+
+                    b.Draw(
+                        texture: ModEntry.EventSprites,
+                        position: position
+                            + new Vector2(0.5f, -0.25f) * Game1.tileSize * mult
+                            + new Vector2(0f, ratio * Game1.tileSize * mult * 0.75f),
+                        sourceRectangle: source,
+                        color: Color.White
+                            * spriteAlpha
+                            * circular,
+                        rotation: MathF.PI * 2 * ratio,
+                        origin: source.Size.ToVector2() / 2,
+                        scale: scale,
+                        effects: SpriteEffects.None,
+                        layerDepth: 1);
+                }
+            }
         }
     }
 }
---
+++ Hikawa/Objects/Events/Noodles.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace Hikawa.Objects.Events
+{
+    public class Noodles : ICustomEventScript
+    {
+        readonly NPC Rei;
+        int Stage;
+        float Timer;
+        float Alpha;
+        float AlphaChange;
+        bool CanQuit;
+
+        public Noodles()
+        {
+            Rei = Game1.CurrentEvent.getActorByName(ModEntry.ModData.NpcRei);
+        }
+
+        public void draw(SpriteBatch b)
+        {
+        }
+
+        public void drawAboveAlwaysFront(SpriteBatch b)
+        {
+            float ms = (float)Game1.currentGameTime.TotalGameTime.TotalMilliseconds;
+            float interval = MathF.PI * 1000;
+
+            float spriteAlpha = 1f * this.Alpha;
+            float overlayAlpha = 0.5f * this.Alpha;
+            float scale = Game1.pixelZoom;
+
+            bool[] big = [true, true, true, false, false];
+            Rectangle[][] ingredients = [
+                [
+                    // fishcake
+                    new(0, 1184, 32, 32)
+                ],
+                [
+                    // egg
+                    new(32, 1184, 32, 32)
+                ],
+                [
+                    // seaweed
+                    new(64, 1184, 32, 32)
+                ],
+                [
+                    // spring onions
+                    new(96, 1184, 16, 16), new(96, 1200, 16, 16), new(112, 1184, 16, 16)
+                ],
+                [
+                    // frozen veg
+                    new(112, 1200, 16, 16), new(128, 1184, 16, 16), new(128, 1200, 16, 16), new(128, 1200, 16, 16), new(144, 1200, 16, 16), new(144, 1200, 16, 16), new(160, 1200, 16, 16), new(160, 1200, 16, 16)
+                ]
+            ];
+
+            // Draw overlay
+            b.Draw(Game1.staminaRect, new Rectangle(0, 0, Game1.viewport.Width, Game1.viewport.Height), Color.OrangeRed * overlayAlpha);
+
+            if (this.Stage < 0 || this.Stage >= ingredients.Length)
+                return;
+
+            int mult = big[this.Stage] ? 6 : 3;
+            int w = Game1.viewport.Width;
+            int h = Game1.viewport.Height;
+            int countX = w / Game1.tileSize / mult;
+            int countY = h / Game1.tileSize / mult + 1;
+
+            // Draw ingredient sprites
+            for (int x = 0; x < countX; ++x)
+            {
+                for (int y = 0; y < countY; ++y)
+                {
+                    float ratio = ((ms + (x + y) * 1000) % interval) / interval;
+                    float circular = Utils.SharpCircularFromRatio(ratio);
+
+                    Rectangle[] sources = ingredients[this.Stage];
+                    Rectangle source = sources[(x + y) % sources.Length];
+                    Vector2 position = new Vector2(w * x / w * Game1.tileSize * mult, h * y / h * Game1.tileSize * mult * 1.25f);
+
+                    b.Draw(
+                        texture: ModEntry.EventSprites,
+                        position: position
+                            + new Vector2(0.5f, -0.5f) * Game1.tileSize * mult
+                            + new Vector2(0f, ratio * Game1.tileSize * mult * 0.75f),
+                        sourceRectangle: source,
+                        color: Color.White
+                            * spriteAlpha
+                            * circular,
+                        rotation: MathF.PI * 2 * ratio,
+                        origin: source.Size.ToVector2() / 2,
+                        scale: scale,
+                        effects: SpriteEffects.None,
+                        layerDepth: 1);
+                }
+            }
+
+            // Redraw actors above overlay and sprites
+            foreach (var actor in Game1.CurrentEvent.actors)
+                actor.draw(b);
+            foreach (var actor in Game1.CurrentEvent.farmerActors)
+                actor.draw(b);
+        }
+
+        public bool update(GameTime time, Event e)
+        {
+            float ms = (float)time.ElapsedGameTime.TotalMilliseconds;
+
+            this.Timer = Math.Max(this.Timer - ms, 0);
+            this.Alpha = Math.Clamp(this.Alpha + this.AlphaChange * ms, 0, 1);
+
+            if (e.spriteTextToDraw != null)
+            {
+                e.int_useMeForAnything2 = 1;
+                e.float_useMeForAnything += ms;
+
+                if (e.float_useMeForAnything > 80f)
+                {
+                    if (e.int_useMeForAnything >= e.spriteTextToDraw.Length)
+                    {
+                        if (e.float_useMeForAnything >= 2500)
+                        {
+                            e.int_useMeForAnything = 0;
+                            e.float_useMeForAnything = 0;
+                            e.spriteTextToDraw = "";
+                            e.spriteTextToDraw = null;
+                        }
+                    }
+                    else
+                    {
+                        e.int_useMeForAnything++;
+                        e.float_useMeForAnything = 0f;
+                    }
+                }
+            }
+
+            switch (this.Stage)
+            {
+                case 0:
+                    this.AlphaChange = 0.001f;
+                    if (this.Alpha >= 1)
+                    {
+                        e.spriteTextToDraw = "fishcake...";
+                        this.Timer = 3000;
+                        ++this.Stage;
+                    }
+                    break;
+                case 1:
+                    this.AlphaChange = 0;
+                    if (this.Timer <= 0)
+                    {
+                        e.spriteTextToDraw = "egg...";
+                        this.Timer = 3000;
+                        ++this.Stage;
+                    }
+                    break;
+                case 2:
+                    this.AlphaChange = 0;
+                    if (this.Timer <= 0)
+                    {
+                        e.spriteTextToDraw = "seaweed...";
+                        this.Timer = 3000;
+                        ++this.Stage;
+                    }
+                    break;
+                case 3:
+                    this.AlphaChange = 0;
+                    if (this.Timer <= 0)
+                    {
+                        e.spriteTextToDraw = "sprongions...";
+                        this.Timer = 3000;
+                        ++this.Stage;
+                    }
+                    break;
+                case 4:
+                    this.AlphaChange = 0;
+                    if (this.Timer <= 0)
+                    {
+                        e.spriteTextToDraw = "fregetables...";
+                        this.Timer = 3000;
+                        ++this.Stage;
+                    }
+                    break;
+                case 5:
+                    this.AlphaChange = -0.001f;
+                    if (this.Alpha == 0)
+                    {
+                        e.spriteTextToDraw = null;
+                        ++this.Stage;
+                    }
+                    break;
+                default:
+                    this.CanQuit = true;
+                    break;
+            }
+
+            return this.CanQuit;
+        }
+    }
+}
