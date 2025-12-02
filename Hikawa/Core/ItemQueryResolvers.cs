d86fe360a9f13dcea794bc3dbcad57faec21bb0b
blueberry <dearbblueberry@gmail.com>
2025-12-02 23:14:52 +1100

    Add Geode to ItemQueryResolvers

--- Hikawa/Core/ItemQueryResolvers.cs
+++ Hikawa/Core/ItemQueryResolvers.cs
@@ -23,5 +23,19 @@ namespace Hikawa
                     ItemQueryResolver.Register($"{prefix}_{itemQueryResolver.Id}", method.CreateDelegate<ResolveItemQueryDelegate>());
             }
         }
+
+        [ItemQueryAttribute("Geode")]
+        private static IEnumerable<ItemQueryResult> Geode(string key, string arguments, ItemQueryContext context, bool avoidRepeat, HashSet<string> avoidItemIds, Action<string, string> logError)
+        {
+            if (string.IsNullOrWhiteSpace(arguments))
+            {
+                return Helpers.ErrorResult(key, arguments, logError, "must specify an item ID");
+            }
+            if (Utility.getTreasureFromGeode(ItemRegistry.Create(arguments)) is Item output)
+            {
+                return [new ItemQueryResult(output)];
+            }
+            return [];
+        }
     }
 }
