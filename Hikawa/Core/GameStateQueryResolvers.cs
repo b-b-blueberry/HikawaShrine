4716cc482803e2317e39a16f8d65381745665b04
blueberry <dearbblueberry@gmail.com>
2026-01-22 12:53:00 +1100

    Add BugCollection GSQ delegates

--- Hikawa/Core/GameStateQueryResolvers.cs
+++ Hikawa/Core/GameStateQueryResolvers.cs
@@ -21,5 +21,32 @@ namespace Hikawa
                     GameStateQuery.Register($"{prefix}_{gameStateQuery.Id}", method.CreateDelegate<GameStateQueryDelegate>());
             }
         }
+
+        [GameStateQueryAttribute("BugsCaught")]
+        private static bool BugsCaught(string[] args, GameStateQueryContext context)
+        {
+            int min = ArgUtility.GetInt(args, 0, 0);
+            int max = ArgUtility.GetInt(args, 1, int.MaxValue);
+            int count = ModEntry.SaveData.BugCollection.Select(bug => bug.Value.Count).Sum();
+            return count >= min && count <= max;
+        }
+
+        [GameStateQueryAttribute("BugTypesCaught")]
+        private static bool BugTypesCaught(string[] args, GameStateQueryContext context)
+        {
+            int min = ArgUtility.GetInt(args, 0, 0);
+            int max = ArgUtility.GetInt(args, 1, int.MaxValue);
+            int count = ModEntry.SaveData.BugCollection.Select(bug => bug.Value.Count > 0).Count();
+            return count >= min && count <= max;
+        }
+
+        [GameStateQueryAttribute("BugCollectionProgress")]
+        private static bool BugCollectionProgress(string[] args, GameStateQueryContext context)
+        {
+            float min = ArgUtility.GetFloat(args, 0, 0);
+            float max = ArgUtility.GetFloat(args, 1, 1);
+            float progress = ModEntry.SaveData.BugCollection.Select(bug => bug.Value.Count > 0).Count() / ModEntry.BugsData.Value.BugData.Count;
+            return progress >= min && progress <= max;
+        }
     }
 }
