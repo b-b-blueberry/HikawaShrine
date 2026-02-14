486d307fa8bbba0944ff8da22549f117e682b238
blueberry <dearbblueberry@gmail.com>
2026-02-14 18:01:24 +1100

    update island + mirage maps with bamboo tiles and outdoors tsx files

---
+++ [CP] Hikawa/assets/Locations/outdoors2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="outdoors2" tilewidth="16" tileheight="16" tilecount="1120" columns="16">
+ <image source="summer_outdoorsTileSheet2" width="256" height="1120"/>
+</tileset>
