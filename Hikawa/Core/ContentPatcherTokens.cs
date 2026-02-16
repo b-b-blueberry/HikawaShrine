012727ed1155b42a07e2fd7b2d1571cc458958bd
blueberry <dearbblueberry@gmail.com>
2026-02-17 00:52:08 +1100

    draft shop wedding boutique implementation and dialogue

--- Hikawa/Core/ContentPatcherTokens.cs
+++ Hikawa/Core/ContentPatcherTokens.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using StardewValley.GameData.Characters;
+using StardewValley.GameData.Locations;
+using StardewValley.TokenizableStrings;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace Hikawa
@@ -25,6 +29,48 @@ namespace Hikawa
         private static IEnumerable<string> SeasonalOutfits()
         {
             return [ModEntry.Config.SeasonalOutfits.ToString()];
+        }
+
+        [ContentPatcherTokenAttribute("WeddingLocations")]
+        private static IEnumerable<string> WeddingLocations()
+        {
+            if (ModEntry.ModData is null)
+                yield break;
+
+            yield return "Town";
+            yield return ModEntry.ModData.MapShrine;
+            if (Game1.player.locationsVisited.Contains(ModEntry.ModData.MapIsland))
+                yield return ModEntry.ModData.MapIsland;
+        }
+
+        [ContentPatcherTokenAttribute("WeddingLocationsResponses")]
+        private static IEnumerable<string> WeddingLocationsResponses()
+        {
+            if (ModEntry.ModData is null)
+                yield break;
+
+            foreach (var id in WeddingLocations())
+                yield return $"#$r {id} 0 wedding_who#{(Game1.locationData.TryGetValue(id, out LocationData data) ? TokenParser.ParseText(data.DisplayName) : id)}"; // TODO: add flag/ct
+        }
+
+        [ContentPatcherTokenAttribute("WeddingOfficiantsResponses")]
+        private static IEnumerable<string> WeddingOfficiantsResponses()
+        {
+            if (ModEntry.ModData is null)
+                yield break;
+
+            foreach (var id in new[] { "Lewis", ModEntry.ModData.NpcRei, ModEntry.ModData.NpcGramps })
+                yield return $"#$r {id} 0 wedding_how#{(Game1.characterData.TryGetValue(id, out CharacterData data) ? TokenParser.ParseText(data.DisplayName) : id)}"; // TODO: add flag/ct
+        }
+
+        [ContentPatcherTokenAttribute("WeddingStyleResponses")]
+        private static IEnumerable<string> WeddingStyleResponses()
+        {
+            if (ModEntry.ModData is null)
+                yield break;
+
+            foreach (var id in new[] { "default", "shinto" })
+                yield return $"#$r {id} 0 wedding_confirmed#{ModEntry.I18n.Get($"ui.shop.wedding.options.how.{id}")}"; // TODO: add flag/ct
         }
     }
 }
