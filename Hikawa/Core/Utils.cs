3edcd4e899676d15d785a2f512373565e8916b0a
blueberry <dearbblueberry@gmail.com>
2026-03-03 18:34:45 +1100

    raise error on hearthlight failed to add

--- Hikawa/Core/Utils.cs
+++ Hikawa/Core/Utils.cs
@@ -298,7 +298,10 @@ namespace Hikawa
 							color: entry.Color);
 						++j;
 					}
-					where.sharedLights.TryAdd(light.Id, light);
+					if (!where.sharedLights.TryAdd(light.Id, light))
+					{
+						Log.E($"Failed to add light {light.Id} ({i} - {entry.TextureName ?? "null"}) at {entry.Tile} to {where.Name}");
+					}
 				}
 			}
 		}
